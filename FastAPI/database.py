446fbdc0729610a311ace322af410501094b3f97
peter-njuku <137099178+peter-njuku@users.noreply.github.com>
2025-05-30 11:12:10 +0300

    Update database.py

--- FastAPI/database.py
+++ FastAPI/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = 'postgresql://peter:Peter@localhost:5432/transactions'
+DATABASE_URL = 'sqlite:///./transactions'
 
 engine= create_engine(DATABASE_URL)
 
